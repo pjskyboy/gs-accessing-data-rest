f7ff59a93d7edeb4e5b118493abf0987e640c866
pjskyboy <pjskyboy@gmail.com>
2017-04-04 01:18:47 +0100

    Builds RDS aurora cluster, generates application.yaml as part of EC2 start up using described RDS outputs.

--- complete/src/deploy/aws/create-project-stack.bash
+++ complete/src/deploy/aws/create-project-stack.bash
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 -p project -a azRoot -k publicKeyname [-f yaml_filenames]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -p project -a azRoot -k publicKeyname [-f yaml_filenames] [-u cluster-username] [-c cluster-password] [-j jdbcUrl]" 1>&2; exit 1; }
 
 project=""
 azRoot=""
 publicKeyname=""
 fileList="network.yml rds.yml ec2.yml"
+username="ec2appdbo"
+credential=""
+jdbcUrl="missing"
 
-while getopts ":p:a:k:f:" o; do
+while getopts ":p:a:k:f:u:c:j:" o; do
     case "${o}" in
+        j)
+            jdbcUrl=${OPTARG}
+            ;;
+        u)
+            username=${OPTARG}
+            ;;
+        c)
+            credential=${OPTARG}
+            ;;
         p)
             project=${OPTARG}
             ;;
@@ -36,17 +48,23 @@ echo "project [${project}]"
 echo "azRoot [${azRoot}]"
 echo "publicKeyname [${publicKeyname}]"
 echo "fileList [${fileList}]"
+echo "username [${username}]"
+echo "credential [${credential}]"
+echo "jdbcUrl [${jdbcUrl}]"
 
 # eu-west-1 ami id
 amiId="ami-2587b443"
-ebsSnapshotId="snap-013cb8e91e836e53c"
+ebsSnapshotId="snap-0853d33212ae1395d"
 
 if [ "${azRoot}" == "eu-west-2" ]
 then
     # eu-west-2 resources
     amiId="ami-0eacb96a"
-    ebsSnapshotId="snap-0261788f106d8e63a"
+#    ebsSnapshotId="snap-0261788f106d8e63a"
+    echo "*** You need to replicate the ${ebsSnapshotId} to eu-west-2! ***"
+    exit 200
 fi
+
 echo "amiId [${amiId}]"
 echo "ebsSnapshotId [${ebsSnapshotId}]"
 
@@ -66,10 +84,10 @@ do
             parameterList="ParameterKey=projectName,ParameterValue=${project} ParameterKey=azRoot,ParameterValue=${azRoot} ParameterKey=domainName,ParameterValue=${azRoot}.compute.internal"
             ;;
         "ec2.yml")
-            parameterList="ParameterKey=projectName,ParameterValue=${project} ParameterKey=networkStackName,ParameterValue=${project}-network ParameterKey=rdsStackName,ParameterValue=${project}-rds ParameterKey=amiId,ParameterValue=${amiId} ParameterKey=keyPair,ParameterValue=${publicKeyname} ParameterKey=ownerId,ParameterValue=751191391887 ParameterKey=ebsSnapshotId,ParameterValue=${ebsSnapshotId} "
+            parameterList="ParameterKey=projectName,ParameterValue=${project} ParameterKey=networkStackName,ParameterValue=${project}-network ParameterKey=rdsStackName,ParameterValue=${project}-rds ParameterKey=amiId,ParameterValue=${amiId} ParameterKey=keyPair,ParameterValue=${publicKeyname} ParameterKey=ownerId,ParameterValue=751191391887 ParameterKey=ebsSnapshotId,ParameterValue=${ebsSnapshotId} ParameterKey=clusterUsername,ParameterValue=${username} ParameterKey=clusterPassword,ParameterValue=${credential}  ParameterKey=jdbcUrl,ParameterValue=${jdbcUrl}"
             ;;
         "rds.yml")
-            parameterList="ParameterKey=projectName,ParameterValue=${project} ParameterKey=networkStackName,ParameterValue=${project}-network ParameterKey=azRoot,ParameterValue=${azRoot}"
+            parameterList="ParameterKey=projectName,ParameterValue=${project} ParameterKey=networkStackName,ParameterValue=${project}-network ParameterKey=azRoot,ParameterValue=${azRoot} ParameterKey=clusterUsername,ParameterValue=${username} ParameterKey=clusterPassword,ParameterValue=${credential}"
             ;;
     esac
 
@@ -77,6 +95,12 @@ do
     echo "parameterList [${parameterList}]"
 
     aws cloudformation create-stack --stack-name ${stackName} --template-body file://yaml.d/${yaml} --parameters ${parameterList}
+
+    if [ ${yaml} == "rds.yml" ]
+    then
+        jdbcUrl=$(aws cloudformation describe-stacks --stack-name ${project}-rds --query 'Stacks[0].Outputs[?OutputKey==`clusterJDBCConnectionString`].OutputValue' --output text)
+        echo "jdbcUrl [${jdbcUrl}]"
+    fi
 
     if [ $? == 0 ]
     then
@@ -95,5 +119,3 @@ done
 echo "Execution completed"
 
 popd
-
-
